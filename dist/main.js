44caab58de3dfefbc6063601bfa93df7c94a1c55
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-26 19:39:10 -0400

    Change "example" const name to "headline"

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,10 @@
 alert("oh hell /:");
 
 window.onload=function(){
-    const example = document.getElementById('h1');
+    const headline = document.getElementById('h1');
     const h1 = document.createElement('h1');
     h1.innerText = "Dreamy Oyster Bar";
-    example.appendChild(h1);
+    headline.appendChild(h1);
+
+
 }
