5390c49dc4205392cc03e2251e0a609f9b7bae18
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-26 19:37:52 -0400

    Change div id for h1

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,7 @@
 alert("oh hell /:");
 
 window.onload=function(){
-    const example = document.getElementById('uuuuum');
+    const example = document.getElementById('h1');
     const h1 = document.createElement('h1');
     h1.innerText = "Dreamy Oyster Bar";
     example.appendChild(h1);
