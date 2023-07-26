bb363046cfa2fc5aa2fec5eb44a7196ed11c8049
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-26 19:35:56 -0400

    Try to add header with js

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,8 @@
 alert("oh hell /:");
 
 window.onload=function(){
-    
+    const example = document.getElementById('uuuuum');
+    const h1 = document.createElement('h1');
+    h1.innerText = "Dreamy Oyster Bar";
+    example.appendChild(h1);
 }
