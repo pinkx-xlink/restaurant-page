bc6470a23e647ce90076cf43364cf158a318528f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-26 20:40:10 -0400

    Create a module w content, try to import

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,15 @@
 alert("oh hell /:");
 
-window.onload=function(){
-    const headline = document.getElementById('h1');
-    const h1 = document.createElement('content');
-    h1.innerText = "Dreamy Oyster Bar";
-    content.appendChild(h1);
+import function loadPage() from "page.js";
 
-    const img = document.createElement("img");
-    img.src = "./oysters.jpg";
-    const src = document.getElementById("content");
-    src.appendChild(img);
-}
+// window.onload=function(){
+//     const headline = document.getElementById('h1');
+//     const h1 = document.createElement('content');
+//     h1.innerText = "Dreamy Oyster Bar";
+//     content.appendChild(h1);
+
+//     const img = document.createElement("img");
+//     img.src = "./oysters.jpg";
+//     const src = document.getElementById("content");
+//     src.appendChild(img);
+// }
--- dist/page.js
+++ dist/page.js
@@ -1,3 +1,12 @@
-const h1Tag = document.createElement('H1');
-h1Tag.innerHTML = "Dreamy Oyster Bar <3";
-$('.output').prepend(h1Tag);
+
+export function loadPage(){
+    const headline = document.getElementById('h1');
+    const h1 = document.createElement('content');
+    h1.innerText = "Dreamy Oyster Bar";
+    content.appendChild(h1);
+
+    const img = document.createElement("img");
+    img.src = "./oysters.jpg";
+    const src = document.getElementById("content");
+    src.appendChild(img);
+}
