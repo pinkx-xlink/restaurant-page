369304128860feec030a23715af599ebf375c3d8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-07-26 21:01:21 -0400

    Comment out Module content

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,21 @@
 alert("oh hell /:");
 
-import function loadPage() from "page.js";
+
+window.onload=function loadPage() {
+    const headline = document.getElementById('h1');
+    const h1 = document.createElement('content');
+    h1.innerText = "Dreamy Oyster Bar";
+    content.appendChild(h1);
+
+    const img = document.createElement("img");
+    img.src = "./oysters.jpg";
+    const src = document.getElementById("content");
+    src.appendChild(img);
+}
+
+// import {loadPage} from "./page.js"
+
+// function loadPage();
 
 // window.onload=function(){
 //     const headline = document.getElementById('h1');
@@ -13,3 +28,5 @@ import function loadPage() from "page.js";
 //     const src = document.getElementById("content");
 //     src.appendChild(img);
 // }
+
+
--- dist/page.js
+++ dist/page.js
@@ -1,14 +1,14 @@
 
-function loadPage() {
-    const headline = document.getElementById('h1');
-    const h1 = document.createElement('content');
-    h1.innerText = "Dreamy Oyster Bar";
-    content.appendChild(h1);
+// function loadPage() {
+//     const headline = document.getElementById('h1');
+//     const h1 = document.createElement('content');
+//     h1.innerText = "Dreamy Oyster Bar";
+//     content.appendChild(h1);
 
-    const img = document.createElement("img");
-    img.src = "./oysters.jpg";
-    const src = document.getElementById("content");
-    src.appendChild(img);
-}
+//     const img = document.createElement("img");
+//     img.src = "./oysters.jpg";
+//     const src = document.getElementById("content");
+//     src.appendChild(img);
+// }
 
 // export default function loadPage();
